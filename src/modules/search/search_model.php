76055095337a33d615b243d00b1c25d2a7e3587d
Grant Sturgill <grantsturgill@gmail.com>
2019-03-19 17:03:48 -0400

    6.3.1c
    Adding search results to search page

--- src/modules/search/search_model.php
+++ src/modules/search/search_model.php
@@ -8,7 +8,9 @@ function queryUserBySkill($skill) {
   try {
     $base = Connector::getDatabase();
 
-    $sql = "SELECT u.UID, CONCAT(u.first_name,' ',u.last_name) AS name, u.photo FROM user AS u WHERE u.UID IN
+    $sql = "SELECT u.UID, CONCAT(u.first_name,' ',u.last_name) AS name,
+			  IFNULL(user.photo, '../../../assets/images/uploads/NoUpload.png') AS photo
+	 		  FROM user AS u WHERE u.UID IN
               (SELECT uss.user_id FROM
                 user_software_skills AS uss JOIN software_skills AS ss
                 ON (uss.skill_id = ss.UID AND ss.skill = :skill));";
@@ -16,20 +18,22 @@ function queryUserBySkill($skill) {
     $stmt = $base->prepare($sql);
     $stmt->bindParam(':skill',$skill,PDO::PARAM_STR);
     $stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $result;
 
-    $rows = [];
-    $i = 0;
-    foreach($stmt as $row){
-      $rows[$i] = array($row['UID'],$row['name'],$skill,$row['photo']);
-      $i++;
-    }
-
-    if(!empty($rows)){
-      return $rows;
-    }
-    else {
-      return null;
-    }
+    // $rows = [];
+    // $i = 0;
+    // foreach($stmt as $row){
+    //   $rows[$i] = array($row['UID'],$row['name'],$skill,$row['photo']);
+    //   $i++;
+    // }
+	//
+    // if(!empty($rows)){
+    //   return $rows;
+    // }
+    // else {
+    //   return null;
+    // }
 
     // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
     // if($result){
