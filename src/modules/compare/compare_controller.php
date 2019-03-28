3ff29fc28bc4f0f03161d335d87b39521bd3a454
Angelina Grosso <44502372+angelinagrosso@users.noreply.github.com>
2019-03-27 20:23:30 -0400

    reorganized compare to be in better model-view-controller (#6)

--- src/modules/compare/compare_controller.php
+++ src/modules/compare/compare_controller.php
@@ -1,28 +1,7 @@
 <?php
 include("compare_model.php");
 
-function Create_Cards($uid){
-	//get user ids
-
-
-	//put cards in overall container and start list for cards
-	?> <div class="container horizontal-scroll">
-			<div class="row">
-				<u1 class="list-inline"> <?php
-
-				//make card for every user id
-				for( $i = 0; $i < count($uid); $i++)
-				{
-					?>
-					<li class = "list-inline-item">
-						<?php individualCard($uid[$i]); ?>
-					</li>
-					<?php
-				}
-	?> </div>
-		</div> <?php
-}
-
+/*
 function individualCard($user) {
 
 	//set booleans for display to be true
@@ -45,7 +24,64 @@ function individualCard($user) {
     </div>
 </div>
 
-
+ */
+?>
 <?php
+
+
+function showName($user)
+{
+	$result = getName($user);
+	echo $result['first_name'] . " " .$result['last_name'];
+}
+
+function showBasic($user, $show_basic)
+{
+	if($show_basic)
+	{
+		$result = getBasic($user);
+		 echo "<hr>";
+                 echo "<strong>Email:</strong> " .$result['email'] ."<br>";
+                 echo "<strong>Gender:</strong> " .$result['gender'] ."<br>" ;
+                 echo "<strong>Location:</strong> " .$result['city'] .", " .$result['state'];
+	}
+}
+
+function showSoft($user, $show_soft)
+{
+	if($show_soft)
+	{
+		$result = getSoft($user);
+		if ($result) {
+                                $MAX_SKILLS_LIST = 5;
+                                echo "<hr>";
+                                echo "<strong>Top Software Skills:</strong><br>";
+                                for ($i = 0; $i < sizeof($result) && $i < $MAX_SKILLS_LIST; $i++) #REMOVED <=
+                                {
+                                        echo $result[$i][0] ."<br>";
+                                }
+                                if (sizeof($result) >= $MAX_SKILLS_LIST) { echo "...";}
+                       }
+	}
+}
+
+function showHard($user, $show_hard)
+{
+	if($show_hard)
+	{
+		$result = getHard($user);
+		if ($result) 
+		{
+			$MAX_SKILLS_LIST = 5;
+                        echo "<hr>";
+			echo "<strong>Top Hardware Skills:</strong><br>";
+			for ($i = 0; $i < sizeof($result) && $i < $MAX_SKILLS_LIST; $i++) #REMOVED <=
+			{
+				echo $result[$i][0] ."<br>";
+			}
+			if (sizeof($result) >= $MAX_SKILLS_LIST) { echo "...";}
+                }
+
+	}
 }
 ?>
