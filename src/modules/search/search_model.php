98ae9901fb592ad63fa7e1a61c2b5dd396f9fe94
LexiKahwaji <lexi.michel@gmail.com>
2019-04-15 22:35:40 -0400

    removed some commented out code

--- src/modules/search/search_model.php
+++ src/modules/search/search_model.php
@@ -11,19 +11,6 @@ function queryUserBySkill($skill) {
   try {
     $base = Connector::getDatabase();
 
-	//Select users from databsae that fit search criteria
-    // $sql = "SELECT u.UID AS userID, CONCAT(u.first_name,' ',u.last_name) AS name,
-	// 		  IFNULL(u.photo, '../../../assets/images/uploads/NoUpload.png') AS photo
-	//  		  FROM user AS u WHERE u.UID IN
-    //           (SELECT uss.user_id FROM
-    //             user_software_skills AS uss JOIN software_skills AS ss
-    //             ON (uss.skill_id = ss.UID AND ss.skill = :skill)
-    //            UNION
-    //            SELECT uhs.user_id FROM
-    //             user_hardware_skills AS uhs JOIN hardware_skills AS hs
-    //             ON (uhs.skill_id = hs.UID AND hs.skill = :skill)
-    //           );";
-
 	$sql = "SELECT u.UID AS userID, CONCAT(u.first_name,' ',u.last_name) AS name,
 				IFNULL(u.photo, '../../../assets/images/uploads/NoUpload.png') AS photo,
 				s_tbl.skill_exp as skill_exp,
@@ -45,20 +32,6 @@ function queryUserBySkill($skill) {
     $stmt->execute();
 	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	return $result;
-
-    // $rows = [];
-    // $i = 0;
-    // foreach($stmt as $row){
-    //   $rows[$i] = array($row['UID'],$row['name'],$skill,$row['photo']);
-    //   $i++;
-    // }
-	//
-    // if(!empty($rows)){
-    //   return $rows;
-    // }
-    // else {
-    //   return null;
-    // }
 
   } catch (Exception $e) {
     throw ($e);
